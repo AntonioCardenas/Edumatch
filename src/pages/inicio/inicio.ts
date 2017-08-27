a61e494b95cb4781282e4b2afa5e39c534262661
AntonioCardenas <antonioalx66@gmail.com>
2017-08-27 03:44:32 -0600

    cats2

--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { InicioProvider } from '../../providers/inicio'
 
-/**
- * Generated class for the InicioPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-inicio',
   templateUrl: 'inicio.html',
 })
 export class InicioPage {
+	private mode: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public InicioProvider: InicioProvider) {
+     this.InicioProvider.setNavController(this.navCtrl);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad InicioPage');
   }
 
 }
--- src/providers/inicio.ts
+++ src/providers/inicio.ts
@@ -2,14 +2,57 @@ import { Injectable,NgZone } from '@angular/core';
 import * as firebase from 'firebase';
 import { Inicio } from '../inicio';
 import { NavController } from 'ionic-angular';
-import 'rxjs/add/operator/map';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { OauthCordova } from 'ng2-cordova-oauth/platform/cordova';
 
 @Injectable()
 export class InicioProvider {
-	private navCtrl: NavController;
+  private oauth: OauthCordova;
+  private navCtrl: NavController;
 
-  constructor(public zone: NgZone) {
-    console.log('Hello InicioProvider Provider');
+  constructor(public zone: NgZone, public googlePlus: GooglePlus) { 
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+            if (user["emailVerified"]) {
+              this.zone.run(() => {
+                this.navCtrl.setRoot(Inicio.homePage, { animate: true });
+              });
+            } else {
+              this.navCtrl.setRoot(Inicio.homePage, { animate: true });
+            }
+          } else {
+            this.zone.run(() => {
+              this.navCtrl.setRoot(Inicio.homePage, { animate: true });
+            });
+          }
+        });
+  }
+
+    googleInicio() {
+    this.googlePlus.Inicio({
+      'webClientId': Inicio.googleClientId
+    }).then((success) => {
+      let credential = firebase.auth.GoogleAuthProvider.credential(success['idToken'], null);
+      firebase.auth().signInWithCredential(credential)
+        .then((success) => {
+        })
+        .catch((error) => {
+          let code = error["code"];
+        });
+    }).catch((error) => {
+      alert(error);
+    });
+  }
+  setNavController(navCtrl) {
+    this.navCtrl = navCtrl;
+  }
+  inicioInvitado() {
+    firebase.auth().signInAnonymously()
+      .then((success) => {
+      })
+      .catch((error) => {
+        let code = error["code"];
+      });
   }
 
 }
