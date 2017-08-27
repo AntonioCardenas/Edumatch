a9533b99325c503a7dfa6e43b0f684a9853e5cdd
AntonioCardenas <antonioalx66@gmail.com>
2017-08-27 04:19:05 -0600

    cats4

--- src/inicio.ts
+++ src/inicio.ts
@@ -2,7 +2,12 @@ import { TabsPage } from './pages/tabs/tabs'
 
 export namespace Inicio {
 export const firebaseConfig = {
-	
+    apiKey: "AIzaSyDZj2hUs6FMogTXtOIVemNkgrKTi0pY0o4",
+    authDomain: "educatech-57e12.firebaseapp.com",
+    databaseURL: "https://educatech-57e12.firebaseio.com",
+    projectId: "educatech-57e12",
+    storageBucket: "educatech-57e12.appspot.com",
+    messagingSenderId: "116703353846"
 };
 
 export const homePage =TabsPage;
